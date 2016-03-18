f1612a5190f61e03d4ca9461995f4e3211715334
Bastien DHIVER <epitech@bastn.fr>
2016-03-18 13:47:28 +0100

    Fix strncmp.S

--- tests/tests_strncmp.c
+++ tests/tests_strncmp.c
@@ -5,7 +5,7 @@
 ** Login   <dhiver_b@epitech.net>
 ** 
 ** Started on  Thu Mar 17 00:20:17 2016 Bastien DHIVER
-** Last update Thu Mar 17 10:36:03 2016 Bastien DHIVER
+** Last update Fri Mar 18 13:41:23 2016 Bastien DHIVER
 */
 
 #include "tests.h"
@@ -13,9 +13,9 @@
 void	tests_strncmp_again(char *str, char *str2)
 {
   str2 = "zbcd";
-  assert(strncmp(str, str2, 4) == -1);
+  assert(strncmp(str, str2, 4) == 'a' - 'z');
   str = "aycd";
-  assert(strncmp(str2, str, 4) == -1);
+  assert(strncmp(str2, str, 4) == 'z' - 'a');
   str = "abcdzz";
   str2 = "abcdzz";
   assert(strncmp(str, str2, 4) == 0);
@@ -31,20 +31,20 @@ void	tests_strncmp(void)
   str = "patate";
   str2 = "z";
   assert(strncmp(str, str, 9999999) == 0);
-  assert(strncmp(str, str2, 0) == 'p' - 'z');
+  assert(strncmp(str, str2, 0) == 0);
   str = "";
   str2 = "";
   assert(strncmp(str, str2, 1) == 0);
   str = "a";
   str2 = "";
-  assert(strncmp(str, str2, 1) == 1);
-  assert(strncmp(str2, str, 1) == -1);
+  assert(strncmp(str, str2, 1) == 'a' - '\0');
+  assert(strncmp(str2, str, 1) == '\0' - 'a');
   str = "a";
   assert(strncmp(str, str, 1) == 0);
   str = "abcd";
   str2 = "ab";
-  assert(strncmp(str, str2, 3) == 1);
-  assert(strncmp(str2, str, 3) == -1);
+  assert(strncmp(str, str2, 3) == 'c' - '\0');
+  assert(strncmp(str2, str, 3) == '\0' - 'c');
   str2 = "abcd";
   assert(strncmp(str, str2, 3) == 0);
   assert(strncmp(str, str2, 4) == 0);
