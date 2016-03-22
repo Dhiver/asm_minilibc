16f1ff841c423e26933ba92b4bd0e56085ae747d
Bastien DHIVER <epitech@bastn.fr>
2016-03-22 16:30:22 +0100

    Fix strlen tests

--- tests/tests_strlen.c
+++ tests/tests_strlen.c
@@ -5,16 +5,25 @@
 ** Login   <dhiver_b@epitech.net>
 ** 
 ** Started on  Mon Mar 14 23:53:25 2016 Bastien DHIVER
-** Last update Tue Mar 15 00:11:30 2016 Bastien DHIVER
+** Last update Tue Mar 22 16:28:31 2016 Bastien DHIVER
 */
 
 #include "tests.h"
 
 void	tests_strlen(void)
 {
-  assert(strlen("") == 0);
-  assert(strlen("a") ==	1);
-  assert(strlen("ab") == 2);
-  assert(strlen("42") == 2);
-  assert(strlen("azertyuiopqsdfghjklmwxcvbn") == 26);
+  char	*str;
+
+  str = "";
+  assert(strlen(str) == 0);
+  str = "    ";
+  assert(strlen(str) == 4);
+  str = "a";
+  assert(strlen(str) ==	1);
+  str = "ab";
+  assert(strlen(str) == 2);
+  str = "42";
+  assert(strlen(str) == 2);
+  str = "azertyuiopqsdfghjklmwxcvbn";
+  assert(strlen(str) == 26);
 }
