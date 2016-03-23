ad3f58e5466dbe0586cf994eee0eae83b3c4b532
Bastien DHIVER <epitech@bastn.fr>
2016-03-24 00:32:02 +0100

    Fix a strchr() test

--- tests/tests_strchr.c
+++ tests/tests_strchr.c
@@ -5,7 +5,7 @@
 ** Login   <dhiver_b@epitech.net>
 ** 
 ** Started on  Tue Mar 15 00:10:32 2016 Bastien DHIVER
-** Last update Tue Mar 15 00:45:24 2016 Bastien DHIVER
+** Last update Thu Mar 24 00:31:26 2016 Bastien DHIVER
 */
 
 #include "tests.h"
@@ -13,9 +13,11 @@
 void	tests_strchr(void)
 {
   char	*toto;
+  char	*empty;
 
   toto = "totoa";
-  assert(strchr("", 'a') == NULL);
+  empty = "";
+  assert(strchr(empty, 'a') == NULL);
   assert(strchr(toto, 0) == &toto[5]);
   assert(strchr(toto, 'z') == NULL);
   assert(strchr(toto, 'o') == &toto[1]);
