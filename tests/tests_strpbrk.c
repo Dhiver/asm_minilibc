3181c6e9b68a3f0986bb0038004c03a7077aefd0
Bastien DHIVER <epitech@bastn.fr>
2016-03-24 00:28:48 +0100

    Add unit tests for strpbrk()

--- tests/tests_strpbrk.c
+++ tests/tests_strpbrk.c
@@ -5,10 +5,28 @@
 ** Login   <dhiver_b@epitech.net>
 ** 
 ** Started on  Mon Mar 21 23:06:11 2016 Bastien DHIVER
-** Last update Tue Mar 22 14:06:02 2016 Bastien DHIVER
+** Last update Thu Mar 24 00:28:08 2016 Bastien DHIVER
 */
 
 #include "tests.h"
+
+void	tests_strpbrk_again(char *str, char *str2)
+{
+  str = "toto";
+  str2 = "o";
+  assert(strpbrk(str, str2) == &str[1]);
+  str2 = "to";
+  assert(strpbrk(str, str2) == str);
+  str2 = "\n";
+  assert(strpbrk(str, str2) == NULL);
+  str = "abcdefghijkl";
+  str2 = "i";
+  assert(strpbrk(str, str2) == &str[8]);
+  str2 = "l";
+  assert(strpbrk(str, str2) == &str[11]);
+  str2 = "";
+  assert(strpbrk(str, str2) == NULL);
+}
 
 void	tests_strpbrk(void)
 {
@@ -29,4 +47,5 @@ void	tests_strpbrk(void)
   str = "trucmachinpatatz";
   str2 = "z";
   assert(strpbrk(str, str2) == &str[15]);
+  tests_strpbrk_again(str, str2);
 }
