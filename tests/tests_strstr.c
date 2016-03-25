dbc091f0fc2b726db0373636eb37bd3f3f65d1c9
Bastien DHIVER <epitech@bastn.fr>
2016-03-25 09:51:41 +0100

    Fix strstr.S

--- tests/tests_strstr.c
+++ tests/tests_strstr.c
@@ -5,7 +5,7 @@
 ** Login   <dhiver_b@epitech.net>
 ** 
 ** Started on  Thu Mar 17 01:29:09 2016 Bastien DHIVER
-** Last update Thu Mar 24 00:14:52 2016 Bastien DHIVER
+** Last update Fri Mar 25 09:51:18 2016 Bastien DHIVER
 */
 
 #include "tests.h"
@@ -17,13 +17,18 @@ void	tests_strstr(void)
 
   str = "cd";
   str2 = "abcdabcd";
-  assert(strstr(NULL, "") == 0);
+  assert(strstr(NULL, "") == NULL);
   assert(strstr(str, str) == str);
   assert(strstr(str2, str) == &str2[2]);
   assert(strstr(&str2[3], str) == &str2[6]);
   str = "zz";
-  assert(strstr(str2, str) == 0);
+  assert(strstr(str2, str) == NULL);
+  str = "toto";
+  assert(strstr(str, str) == str);
+  str = "blaroutin";
+  str2 = "rou";
+  assert(strstr(str, str2) == &str[3]);
   str = "abcdefghijkl";
   str2 = "";
-  //assert(strstr(str, str2) == str);
+  assert(strstr(str, str2) == str);
 }
